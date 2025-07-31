ea2ad2ee0d83bd8acd993d02f8ebffefcc293e73
Derik Martins <147659267+derik-martins@users.noreply.github.com>
2025-07-31 13:59:09 -0300

    Deploy inicial da documentação

---
+++ dados-git-hub.md
@@ -0,0 +1,2 @@
+link org: https://github.com/SISqualis
+repositorio que quero que esat doc seja vista: https://github.com/SISqualis/docs-fusion
--- fusion/docusaurus.config.ts
+++ fusion/docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://sisqualis.github.io', // URL do GitHub Pages
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/doc-fusion/',
+  baseUrl: '/docs-fusion/'
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'SISqualis', // Nome da organização no GitHub
-  projectName: 'doc-fusion', // Nome do repositório
+  projectName: 'docs-fusion', // Nome do repositório
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
