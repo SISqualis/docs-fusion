6d3350838ab1089e54c3b85b63440950cae00f01
Derik Martins <147659267+derik-martins@users.noreply.github.com>
2025-07-31 15:13:06 -0300

    Correção de arquivos de build

--- fusion/docusaurus.config.ts
+++ fusion/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://sisqualis.github.io', // URL do GitHub Pages
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs-fusion/'
+  baseUrl: '/docs-fusion/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
